00b80590e1e6eae30ab67c3d93e81d96d4390692
lloncar <lloncar.vz@gmail.com>
2025-04-16 22:04:32 +0300

    cardAndSetProfiles - added PokemonProfileFactory

---
+++ Assets/Scripts/Data/Profiles/PokemonProfileFactory.cs
@@ -0,0 +1,34 @@
+using System;
+using GimGim.Enums;
+using UnityEngine;
+
+namespace GimGim.Data {
+    /// <summary>
+    /// Creates instances of set profiles and card profiles from a given json.
+    /// </summary>
+    public static class PokemonProfileFactory {
+        public static SetProfile CreateSetProfile() {
+            return new SetProfile();
+        }
+
+        public static DeckProfile CreateDeckProfile() {
+            return new DeckProfile();
+        }
+
+        public static CardProfile CreateCardProfile(string superType) {
+            if (Enum.TryParse(superType, out SuperType type)) {
+                switch (type) {
+                    case SuperType.Energy:
+                        return new EnergyProfile();
+                    case SuperType.Pokemon:
+                        return new PokemonProfile();
+                    case SuperType.Trainer:
+                        return new TrainerProfile();
+                }
+            }
+
+            Debug.Log($"PokemonProfileFactory - Could not parse superType: {superType}");
+            return null;
+        }
+    }
+}

5abec3b5257b73131134758b7d91976ec681968c
lloncar <lloncar.vz@gmail.com>
2025-05-07 23:19:03 +0300

    eventCenter - added EventData class

---
+++ Assets/Scripts/EventCenter/EventData.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace GimGim.EventCenter {
+    /// <summary>
+    /// Base class for game event data objects.
+    /// </summary>
+    public abstract class EventData {
+        private object Sender { get; set; }
+        public HashSet<int> TypeHashes { get; set; }
+
+        public void SetSender(object sender) {
+            Sender = sender;
+            TypeHashes = EventTypeRegistry.GetTypeHashes(GetType());
+        }
+
+        public override string ToString() => $"{GetType().Name} (Sender: {Sender?.GetType().Name ?? "null"})";
+    }
+}

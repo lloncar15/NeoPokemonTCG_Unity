ff69a05998e2d58a8a01faa7599df587b4a42937
lloncar <lloncar.vz@gmail.com>
2025-05-13 16:33:02 +0300

    actionSystem - created a Counter class (name to be changed) for counting order of play

---
+++ Assets/Scripts/Utility/Counter/Counters.cs
@@ -0,0 +1,19 @@
+using System.Threading;
+
+namespace GimGim.Utility.Counter {
+    public class LocalCounter : ICounter {
+        private int _counter = 0;
+        public int Next() => ++_counter;
+    }
+
+    public class ManualCounter : ICounter {
+        private int Counter { get; set; }
+        public int Next() => ++Counter;
+        public void Advance(int advanceFor = 1) => Counter += advanceFor;
+    }
+
+    public class GlobalCounter : ICounter {
+        private int _counter = 0;
+        public int Next() => Interlocked.Increment(ref _counter);
+    }
+}
---
+++ Assets/Scripts/Utility/Counter/ICounter.cs
@@ -0,0 +1,8 @@
+namespace GimGim.Utility.Counter {
+    public interface ICounter {
+        /// <summary>
+        /// Returns a strictly increasing sequential number on each call.
+        /// </summary>
+        int Next();
+    }
+}

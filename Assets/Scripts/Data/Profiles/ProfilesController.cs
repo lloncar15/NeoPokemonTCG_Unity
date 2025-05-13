79276227c6ab558e3f153571005db735b6ec8709
lloncar <lloncar.vz@gmail.com>
2025-05-13 09:48:10 +0300

    actionSystem - added ProfilesLoader to allow for a static instance of ProfilesController to have getters in the Cards model

--- Assets/Scripts/Data/Profiles/ProfilesController.cs
+++ Assets/Scripts/Data/Profiles/ProfilesController.cs
@@ -9,118 +9,60 @@ namespace GimGim.Data {
     /// <summary>
     /// Loads all the data needed for the game and caches it for later use.
     /// </summary>
-    public class ProfilesController : MonoBehaviour {
-        private readonly Dictionary<Type, Dictionary<int, Profile>> _profilesForTypes = new();
-        
+    public class ProfilesController {
+        private Dictionary<Type, Dictionary<int, Profile>> _profilesForTypes = new();
         public Dictionary<Type, Dictionary<int, Profile>> ProfilesForTypes => _profilesForTypes;
         
-        public string setProfilesFilename = "setProfiles";
-        public string deckProfilesFilename = "deckProfiles";
-        public string cardProfilesFilename = "cardProfiles";
+        private static ProfilesController _instance;
+        
+        private bool _isLoaded = false;
 
         /// <summary>
-        /// Loads all required profiles for Pokemon TCG. Set profiles should be loaded first to be able to populate
-        /// card and deck ids into the set profile when loading them.
+        /// Gets the singleton instance of the event system.
         /// </summary>
+        public static ProfilesController Instance {
+            get { return _instance ??= new ProfilesController(); }
+        }
+
         public void LoadPokemonProfiles() {
-            LoadSetProfiles();
-            LoadDeckProfiles();
-            LoadCardProfiles();
-        }
-
-        /// <summary>
-        /// Loads the set profiles and adds them to the profile's dictionary.
-        /// </summary>
-        public void LoadSetProfiles() {
-            string json = GetJsonStringForFile(setProfilesFilename);
-            
-            Dictionary<int, Profile> setProfiles = new();
-            JsonDecoder decoder = new JsonDecoder(json);
-            JSONArray decoderCurrentNode = decoder.CurrentNode as JSONArray;
-            if (decoderCurrentNode != null) {
-                for (int i = 0; i < decoderCurrentNode.Count; i++) {
-                    try {
-                        Profile profile = PokemonProfileFactory.CreateSetProfile();
-                        if (decoder.Get(i, ref profile)) {
-                            setProfiles.Add(i, profile);
-                        }
-                    }
-                    catch (Exception e) {
-                        Debug.Log($"ProfilesController - Could not load set profiles - {e}");
-                    }
-                }
-            }
-            _profilesForTypes[typeof(SetProfile)] = setProfiles;
-        }
-
-        /// <summary>
-        /// Loads the deck profiles and adds them to the profiles dictionary while adding deck profile ids into corresponding sets.
-        /// </summary>
-        public void LoadDeckProfiles() {
-            string json = GetJsonStringForFile(deckProfilesFilename);
-            Dictionary<int, Profile> deckProfiles = new();
-            JsonDecoder decoder = new JsonDecoder(json);
-            JSONArray decoderCurrentNode = decoder.CurrentNode as JSONArray;
-            if (decoderCurrentNode != null) {
-                for (int i = 0; i < decoderCurrentNode.Count; i++) {
-                    try {
-                        DeckProfile profile = PokemonProfileFactory.CreateDeckProfile();
-                        if (decoder.Get(i, ref profile)) {
-                            deckProfiles.Add(i, profile);
-                            // Add the deck profile id to the corresponding set profile
-                            SetProfile setProfile = GetProfile<SetProfile>(profile.SetId);
-                            setProfile?.AddDeckProfile(profile.Id);
-                        }
-                    }
-                    catch (Exception e) {
-                        Debug.Log($"ProfilesController - Could not load deck profiles - {e}");
-                    }
-                }
-            }
-            _profilesForTypes[typeof(DeckProfile)] = deckProfiles;
-        }
-
-        /// <summary>
-        /// Loads the card profiles and adds them to the profiles dictionary while adding card profile ids into corresponding sets.
-        /// </summary>
-        public void LoadCardProfiles() {
-            string json = GetJsonStringForFile(cardProfilesFilename);
-            Dictionary<int, Profile> cardProfiles = new();
-            JsonDecoder decoder = new JsonDecoder(json);
-            JSONArray decoderCurrentNode = decoder.CurrentNode as JSONArray;
-            if (decoderCurrentNode != null) {
-                for (int i = 0; i < decoderCurrentNode.Count; i++) {
-                    try {
-                        string superType = decoderCurrentNode[i]["supertype"];
-                        CardProfile profile = PokemonProfileFactory.CreateCardProfile(superType);
-                        if (decoder.Get(i, ref profile)) {
-                            cardProfiles.Add(i, profile);
-                            // Add the card profile id to the corresponding set profile
-                            SetProfile setProfile = GetProfile<SetProfile>(profile.SetId);
-                            setProfile?.AddCardProfile(profile.Id);
-                        }
-                    }
-                    catch (Exception e) {
-                        Debug.Log($"ProfilesController - Could not load card profile {decoderCurrentNode[i]["id"]} - {e}");
-                    }
-                }
-            }
-            _profilesForTypes[typeof(CardProfile)] = cardProfiles;
+            ProfilesLoader.ProfilesFilenames filenames =
+                new ProfilesLoader.ProfilesFilenames("setProfiles", 
+                    "deckProfiles", 
+                    "cardProfiles");
+            ProfilesLoader loader = new ProfilesLoader(filenames);
+            _profilesForTypes = loader.LoadPokemonProfiles();
+            _isLoaded = true;
         }
         
         #region Profile Getters
-        public T GetProfile<T>(int profileId) where T : Profile {
-            return GetProfile<T>(typeof(T), profileId);
+        public static T GetProfile<T>(int profileId) where T : Profile {
+            return Instance._GetProfile<T>(profileId);
         }
-        private T GetProfile<T>(Type type, int profileId) where T : Profile {
+        public static List<T> GetAllProfiles<T>() where T : Profile {
+            return Instance._GetAllProfiles<T>();
+        }
+        public static Profile GetProfile(Type type, int profileId) {
+            return Instance._GetProfile(type, profileId);
+        }
+        
+        private T _GetProfile<T>(int profileId) where T : Profile {
+            return _GetProfile<T>(typeof(T), profileId);
+        }
+        private T _GetProfile<T>(Type type, int profileId) where T : Profile {
+            EnsureLoaded();
+            
             _profilesForTypes.TryGetValue(type, out Dictionary<int, Profile> profilesForType);
             return profilesForType?.GetValueOrDefault(profileId) as T;
         }
-        public Profile GetProfile(Type type, int profileId) {
+        private Profile _GetProfile(Type type, int profileId) {
+            EnsureLoaded();
+            
             _profilesForTypes.TryGetValue(type, out Dictionary<int, Profile> profilesForType);
             return profilesForType?.GetValueOrDefault(profileId);
         }
-        public List<T> GetAllProfiles<T>() where T : Profile {
+        private List<T> _GetAllProfiles<T>() where T : Profile {
+            EnsureLoaded();
+                
             List<T> profiles = new();
             if (_profilesForTypes.TryGetValue(typeof(T), out Dictionary<int, Profile> profilesForType)) {
                 profiles = profilesForType.Values.Select(profile => profile as T).ToList();
@@ -129,26 +71,9 @@ namespace GimGim.Data {
             return profiles;
         }
         
-        #endregion
-        
-        #region Helpers
-
-        /// <summary>
-        /// Gets the json string from a specified file in the Resources folder.
-        /// </summary>
-        /// <param name="fileName">File name of the specified profiles.</param>
-        /// <returns>The json string in the file.</returns>
-        private string GetJsonStringForFile(string fileName = "") {
-            TextAsset jsonFile = Resources.Load<TextAsset>($"Profiles/{fileName}");
-            string jsonString = null;
-            if (jsonFile is null) {
-                Debug.LogError($"Could not load profile file {fileName}");
-            }
-            else {
-                jsonString = jsonFile.text;
-            }
-
-            return jsonString;
+        private void EnsureLoaded() {
+            if (!_isLoaded)
+                throw new InvalidOperationException("Profiles must be loaded by calling LoadPokemonProfiles() before accessing profiles.");
         }
         
         #endregion
--- Assets/Tests/ProfileLoadingTest.cs
+++ Assets/Tests/ProfileLoadingTest.cs
@@ -1,45 +1,62 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using GimGim.Data;
 using NUnit.Framework;
-using UnityEngine;
 
 public class ProfileLoadingTest
 {
-    private ProfilesController _profilesController;
+    private ProfilesLoader _loader;
+    private Dictionary<Type, Dictionary<int, Profile>> _profilesForTypes = new();
     
     [SetUp]
     public void Setup()
     {
-        _profilesController = new GameObject().AddComponent<ProfilesController>();
-        _profilesController.setProfilesFilename = "TestData/setTestProfiles";
-        _profilesController.deckProfilesFilename = "TestData/deckTestProfiles";
-        _profilesController.cardProfilesFilename = "TestData/cardTestProfiles";
+        ProfilesLoader.ProfilesFilenames filenames =
+            new ProfilesLoader.ProfilesFilenames("TestData/setTestProfiles", 
+                "TestData/deckTestProfiles", 
+                "TestData/cardTestProfiles");
+        _loader = new ProfilesLoader(filenames);
+
+        _profilesForTypes = new Dictionary<Type, Dictionary<int, Profile>>();
     }
 
     [Test]
     public void TestLoadingSetProfiles() {
-        _profilesController.LoadSetProfiles();
-
-        List<SetProfile> setProfiles = _profilesController.GetAllProfiles<SetProfile>();
+        _loader.LoadSetProfiles(ref _profilesForTypes);
+        
+        List<SetProfile> setProfiles = new();
+        if (_profilesForTypes.TryGetValue(typeof(SetProfile), out Dictionary<int, Profile> profilesForType)) {
+            setProfiles = profilesForType.Values.Select(profile => profile as SetProfile).ToList();
+        }
+        
         Assert.AreEqual(2, setProfiles.Count);
         Assert.AreEqual("Base", setProfiles[0].Name);
     }
     
     [Test]
     public void TestLoadingDeckProfiles() {
-        _profilesController.LoadSetProfiles();
-        _profilesController.LoadDeckProfiles();
+        _loader.LoadSetProfiles(ref _profilesForTypes);
+        _loader.LoadDeckProfiles(ref _profilesForTypes);
 
-        List<DeckProfile> deckProfiles = _profilesController.GetAllProfiles<DeckProfile>();
-        Assert.AreEqual(2, _profilesController.ProfilesForTypes.Count);
-        Assert.AreEqual(4, deckProfiles.Count);
+        List<DeckProfile> setProfiles = new();
+        if (_profilesForTypes.TryGetValue(typeof(DeckProfile), out Dictionary<int, Profile> profilesForType)) {
+            setProfiles = profilesForType.Values.Select(profile => profile as DeckProfile).ToList();
+        }
+        
+        Assert.AreEqual(2, _profilesForTypes.Count);
+        Assert.AreEqual(4, setProfiles.Count);
     }
 
     [Test]
     public void TestLoadingCardProfiles() {
-        _profilesController.LoadPokemonProfiles();
+        _profilesForTypes = _loader.LoadPokemonProfiles();
         
-        List<CardProfile> cardProfiles = _profilesController.GetAllProfiles<CardProfile>();
-        Assert.AreEqual(3, _profilesController.ProfilesForTypes.Count);
+        List<CardProfile> cardProfiles = new();
+        if (_profilesForTypes.TryGetValue(typeof(CardProfile), out Dictionary<int, Profile> profilesForType)) {
+            cardProfiles = profilesForType.Values.Select(profile => profile as CardProfile).ToList();
+        }
+        Assert.AreEqual(3, _profilesForTypes.Count);
+        Assert.AreEqual(104, cardProfiles.Count);
     }
 }
